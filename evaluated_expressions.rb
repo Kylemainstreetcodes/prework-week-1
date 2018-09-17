b7653c7e14eccf56e015282caac1e0b9196e3835
Kylemainstreetcodes <kkelley56@gmail.com>
2018-09-17 17:55:16 -0500

    Create evaluated_expressions program

---
+++ evaluated_expressions.rb
@@ -0,0 +1,9 @@
+puts 3+3+3*10 == 36
+puts (2*4)/2-4 == 0
+puts "hello" * 3 == false
+puts "hello" + "3" == false
+puts !true == false
+puts "false" == false
+puts true && !false
+puts false || !true
+puts false || true && 3 == !false
