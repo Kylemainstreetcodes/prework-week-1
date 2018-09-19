5c76f681a360c8beb6a245e792992067422cdf73
Kylemainstreetcodes <kkelley56@gmail.com>
2018-09-19 18:22:26 -0500

    number_multiplier.rb

---
+++ number_multiplier.rb
@@ -0,0 +1,13 @@
+puts "Insert your 1st number NOW!"
+
+number_1 = gets.chomp.to_i
+
+puts "Jam that 2nd number here!"
+
+number_2 = gets.chomp.to_i
+
+puts number_1 * number_2
+
+
+
+puts "Congratulations you filthy animal!"
